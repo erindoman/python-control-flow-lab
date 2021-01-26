ca55da803202378aa3277c9bcab8438144f0b5b0
erindoman <erin.doman@gmail.com>
2021-01-26 13:53:46 -0600

    solve ex 4

--- exercise-4.py
+++ exercise-4.py
@@ -12,3 +12,15 @@
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+print("Enter the lengths of three sides of a triangle: ")
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+if a == b and b == c:
+    print(f"A triangle with sides of {a}, {b}, and {c} is an equilateral triangle!")
+elif a != b and b != c and a != c:
+    print(f"A triangle with sides of {a}, {b}, and {c} is a scalene triangle!")
+else:
+    print(f"A triangle with sides of {a}, {b}, and {c} is an isosceles triangle!")
